072fe167943d8420634f74a45f1cf12f03936815
Victoria Chapman <weyakin@weyakindesigns.com>
2017-04-07 15:40:29 -0400

    Putting site jquery into a separate file so as not to confuse validators. Adding html lang.

---
+++ js/pagejquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#nav-mobile").html($("#nav-main").html());
+    $("#nav-trigger a, #nav-mobile a#closenav").click(function(){
+        if ($("nav#nav-mobile ul").hasClass("expanded")) {
+            $("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+            $(this).removeClass("open");
+        } else {
+            $("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+            $(this).addClass("open");
+        }
+    });
+    $('#nav-trigger a').click(function () {
+        $('#mobile-menu-reader').html($('#mobile-menu-reader').html() == 'Open (Tap to Close)' ? 'Closed (Tap to Open)' : 'Open (Tap to Close)');
+    });
+});
